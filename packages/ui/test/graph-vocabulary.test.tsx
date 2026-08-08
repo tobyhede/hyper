7ebb553e05ad5aceb23d927ce2a6733da8f2b95d
Toby Hede <toby@cipherstash.com>
2026-08-08 19:33:53 +1000

    fix graph presentation consistency

--- packages/ui/test/graph-vocabulary.test.tsx
+++ packages/ui/test/graph-vocabulary.test.tsx
@@ -14,6 +14,7 @@ describe('Graph product vocabulary', () => {
     render(
       <GraphSelector
         graphs={[graph]}
+        colorByGraphId={{}}
         activeGraphId={graph.id}
         onActivate={() => undefined}
         onPresent={() => undefined}
